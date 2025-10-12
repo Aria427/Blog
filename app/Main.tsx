1f3ef4c46244f4fb356a7a668e823659005e1295
Aria427 <saraabdilla274@gmail.com>
2025-10-12 11:17:10 +0200

    Divided all posts & recipes section into two separate ones

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,14 +11,15 @@ import { formatDate } from 'pliny/utils/formatDate';
 
 export const metadata = genPageMetadata({ title: 'Home' });
 
-const MAX_DISPLAY = 6;
+const MAX_DISPLAY = 2;
 
-export default function Home({ posts }) {
+export default function Home({ blogs, recipes }) {
   const author = allAuthors.find((p) => p.slug === 'author/default') as Author;
   const mainContent = coreContent(author);
 
   // Filter out draft posts
-  const filteredPosts = posts.filter((post) => post.draft !== true);
+  const filteredBlogs = blogs.filter((post) => post.draft !== true);
+  const filteredRecipes = recipes.filter((post) => post.draft !== true);
 
   return (
     <>
@@ -29,22 +30,60 @@ export default function Home({ posts }) {
             <MDXLayoutRenderer code={author.body.code} />
           </div>
         </AuthorLayout>
-        {/* Latest List (Right) */}
-        <div className="flex w-full justify-center xl:w-1/3">
-          <div className="mx-auto block h-full max-h-screen max-w-[320px] min-w-[280px] flex-wrap overflow-auto rounded-sm bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40">
+        {/* Latest Lists (Right) */}
+        <div className="flex w-full flex-col gap-8 xl:w-1/3">
+          {/* Latest Posts */}
+          <div className="mx-auto block h-full max-h-[400px] w-full max-w-[320px] min-w-[280px] flex-wrap overflow-auto rounded-sm bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40">
             <div className="px-6 py-4">
-              <h3 className="text-primary-500 mb-4 font-bold uppercase">Latest Posts & Recipes</h3>
+              <h3 className="text-primary-500 mb-4 font-bold uppercase">Latest Posts</h3>
               <ul>
-                {!filteredPosts.length && <li>No posts found.</li>}
-                {filteredPosts.slice(0, MAX_DISPLAY).map((post) => {
-                  const { slug, date, title, type } = post;
-                  const prefix = type === 'Recipes' ? '/recipes/' : '/blog/';
+                {!filteredBlogs.length && <li>No posts found.</li>}
+                {filteredBlogs.slice(0, MAX_DISPLAY).map((post) => {
+                  const { slug, date, title } = post;
                   return (
-                    <li key={`${type || 'post'}-${slug}`} className="mb-6">
+                    <li key={`blog-${slug}`} className="mb-6">
+                      <div className="space-y-1">
+                        <h4 className="text-lg font-bold">
+                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                            {title}
+                          </Link>
+                        </h4>
+                        <div className="text-xs text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+              {filteredBlogs.length > 0 && (
+                <div className="mt-4 flex justify-end text-base leading-6 font-medium">
+                  <Link
+                    href="/blog"
+                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    aria-label="All posts"
+                  >
+                    All Posts &rarr;
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Latest Recipes */}
+          <div className="mx-auto block h-full max-h-[400px] w-full max-w-[320px] min-w-[280px] flex-wrap overflow-auto rounded-sm bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40">
+            <div className="px-6 py-4">
+              <h3 className="text-primary-500 mb-4 font-bold uppercase">Latest Recipes</h3>
+              <ul>
+                {!filteredRecipes.length && <li>No recipes found.</li>}
+                {filteredRecipes.slice(0, MAX_DISPLAY).map((post) => {
+                  const { slug, date, title } = post;
+                  return (
+                    <li key={`recipe-${slug}`} className="mb-6">
                       <div className="space-y-1">
                         <h4 className="text-lg font-bold">
                           <Link
-                            href={`${prefix}${slug}`}
+                            href={`/recipes/${slug}`}
                             className="text-gray-900 dark:text-gray-100"
                           >
                             {title}
@@ -58,14 +97,14 @@ export default function Home({ posts }) {
                   );
                 })}
               </ul>
-              {filteredPosts.length > MAX_DISPLAY && (
+              {filteredRecipes.length > 0 && (
                 <div className="mt-4 flex justify-end text-base leading-6 font-medium">
                   <Link
-                    href="/blog"
+                    href="/recipes"
                     className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label="All posts"
+                    aria-label="All recipes"
                   >
-                    All Posts &rarr;
+                    All Recipes &rarr;
                   </Link>
                 </div>
               )}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { allBlogs, allRecipes } from 'contentlayer/generated';
 import Main from './Main';
 
 export default async function Page() {
-  const sortedPosts = sortPosts([...allBlogs, ...allRecipes]);
-  const posts = allCoreContent(sortedPosts);
-  return <Main posts={posts} />;
+  const sortedBlogs = sortPosts(allBlogs);
+  const sortedRecipes = sortPosts(allRecipes);
+  const blogs = allCoreContent(sortedBlogs);
+  const recipes = allCoreContent(sortedRecipes);
+  return <Main blogs={blogs} recipes={recipes} />;
 }
