64276f5f087a5fb1d2da824313bae5b5dc7e04df
Aria427 <saraabdilla274@gmail.com>
2026-02-07 12:40:41 +0100

    Updates recipe posts to show nutrition information more clearly. Update styling to be more readable as well.

--- components/TableWrapper.tsx
+++ components/TableWrapper.tsx
@@ -1,7 +1,9 @@
 const TableWrapper = ({ children }) => {
   return (
-    <div className="w-full overflow-x-auto">
-      <table>{children}</table>
+    <div className="my-1 flex justify-center">
+      <div className="border-primary-500/30 bg-primary-100 dark:border-primary-500/20 dark:bg-primary-950/20 inline-block overflow-hidden rounded-lg border-2">
+        <table className="m-0 text-sm">{children}</table>
+      </div>
     </div>
   );
 };
